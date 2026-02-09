05728e12e6f978861fa4166287c7684a8fdcf6e7
Joseph Downs <capt1anp3r53u5@gmail.com>
2026-02-09 17:56:42 -0600

    radio highlight

--- Echoes of Belleau/Assets/Scripts/PlayerInteraction.cs
+++ Echoes of Belleau/Assets/Scripts/PlayerInteraction.cs
@@ -10,11 +10,13 @@ public class PlayerInteraction : MonoBehaviour
     [SerializeField] TMP_Text interactPrompt;
 
     IInteractable currentInteractable;
+    IHighlightable currentHighlight;
 
     void Update()
     {
         if (gameManager.instance.isPaused)
         {
+            ClearHighlight();
             HidePrompt();
             return;
         }
@@ -24,20 +26,40 @@ public class PlayerInteraction : MonoBehaviour
         if (currentInteractable != null && Input.GetKeyDown(KeyCode.E))
         {
             currentInteractable.Interact();
+            ClearHighlight();
         }
     }
 
     void CheckForInteractable()
     {
-        currentInteractable = null;
+        IHighlightable newHighlight = null;
+        IInteractable newInteractable = null;
 
         Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
         if (Physics.Raycast(ray, out RaycastHit hit, interactDistance, interactLayer))
         {
-            currentInteractable = hit.collider.GetComponent<IInteractable>();
+            newInteractable = hit.collider.GetComponent<IInteractable>();
+            newHighlight = hit.collider.GetComponent<IHighlightable>();
         }
 
+        if (newHighlight != currentHighlight)
+        {
+            ClearHighlight();
+            currentHighlight = newHighlight;
+            currentHighlight?.Highlight(true);
+        }
+
+        currentInteractable = newInteractable;
         ShowPrompt(currentInteractable != null);
+    }
+
+    void ClearHighlight()
+    {
+        if (currentHighlight != null)
+        {
+            currentHighlight.Highlight(false);
+            currentHighlight = null;
+        }
     }
 
     void ShowPrompt(bool show)
--- Echoes of Belleau/Assets/Scripts/RadioInteractable.cs
+++ Echoes of Belleau/Assets/Scripts/RadioInteractable.cs
@@ -1,18 +1,34 @@
 using UnityEngine;
 
-public class RadioInteractable : MonoBehaviour, IInteractable
+public class RadioInteractable : MonoBehaviour, IInteractable, IHighlightable
 {
+    [SerializeField] Renderer radioRenderer;
+    [SerializeField] Material normalMat;
+    [SerializeField] Material highlightMat;
+
     bool used;
+
+    void Awake()
+    {
+        radioRenderer.material = normalMat;
+    }
 
     public void Interact()
     {
         if (used) return;
         used = true;
 
-        Debug.Log("Radio activated");
-
-        //gameManager.instance.StartTowerDefense();
-
+        Highlight(false);
         GetComponent<Collider>().enabled = false;
+
+        Debug.Log("Radio activated");
+        //defenceManager.instance.StartTowerDefense();
+    }
+
+    public void Highlight(bool enable)
+    {
+        if (used) return;
+
+        radioRenderer.material = enable ? highlightMat : normalMat;
     }
 }
