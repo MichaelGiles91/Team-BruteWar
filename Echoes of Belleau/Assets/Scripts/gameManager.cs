1bc92b87196ec745dfccd7741a0f959260b337c5
Nicolas-Lowery <lowerynicolas@gmail.com>
2026-03-16 20:44:05 -0400

    Added Player Low Health Indicator
    Added Player Low Health Indicator

--- Echoes of Belleau/Assets/Scripts/gameManager.cs
+++ Echoes of Belleau/Assets/Scripts/gameManager.cs
@@ -59,6 +59,7 @@ public class gameManager : MonoBehaviour
     
     public Image playerHPBar;
     public GameObject playerDamageFlash;
+    public Image lowHealthIndicator;
     public Image playerStaminaBar;
     public Image devilDogBarFill;
     [SerializeField] GameObject map;
