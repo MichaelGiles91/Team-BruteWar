c3d520281938e3e2ab964555e3c309fad76a1f90
Austin J Allen <165547446+Dyskro@users.noreply.github.com>
2026-03-17 19:16:50 -0400

    Update SoldierEnemyController.cs
    thx cody

--- Echoes of Belleau/Assets/Scripts/EnemyStateMachine/SoldierEnemyController.cs
+++ Echoes of Belleau/Assets/Scripts/EnemyStateMachine/SoldierEnemyController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -49,6 +50,7 @@ public class SoldierEnemyController : MonoBehaviour, IDamage
     private SoldierStateMachine stateMachine;
     private NavMeshAgent agent;
     private SingleSoldierSpawner spawnPoint;
+    private bool isDead;
 
     public SoldierIdleState IdleState { get; private set; }
     public SoldierPatrolState PatrolState { get; private set; }
@@ -56,6 +58,7 @@ public class SoldierEnemyController : MonoBehaviour, IDamage
     public SoldierSeekCoverState SeekCoverState { get; private set; }
     public SoldierMoveToCoverState MoveToCoverState { get; private set; }
     public SoldierShootState ShootState { get; private set; }
+    public event Action<SoldierEnemyController> OnDied;
 
     private void Awake()
     {
@@ -73,6 +76,7 @@ public class SoldierEnemyController : MonoBehaviour, IDamage
     private void Start()
     {
         currentHealth = maxHealth;
+        isDead = false;
         FindPlayerTarget();
         stateMachine.Initialize(IdleState);
     }
@@ -179,7 +183,7 @@ public class SoldierEnemyController : MonoBehaviour, IDamage
 
         for (int i = 0; i < 10; i++)
         {
-            Vector2 randomCircle = Random.insideUnitCircle * patrolRadius;
+            Vector2 randomCircle = UnityEngine.Random.insideUnitCircle * patrolRadius;
             Vector3 randomPoint = new Vector3(
                 center.x + randomCircle.x,
                 center.y,
@@ -330,6 +334,10 @@ public class SoldierEnemyController : MonoBehaviour, IDamage
     {
         Debug.Log(name + " died.");
 
+        isDead = true;
+
+        OnDied?.Invoke(this);
+
         StopMoving();
 
         if (CurrentCover != null)
@@ -355,11 +363,73 @@ public class SoldierEnemyController : MonoBehaviour, IDamage
         {
             Destroy(gameObject, 2f);
         }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    private void OnDestroy()
+    {
+        if (spawnPoint != null)
+        {
+            spawnPoint.ClearSoldierReference();
+        }
+
+        if (CurrentCover != null)
+        {
+            CurrentCover.isOccupied = false;
+            CurrentCover = null;
+        }
     }
 
     public void takeDamage(int amount)
     {
         TakeDamage(amount);
+    }
+    public void RestoreCheckpointState(Vector3 pos, Quaternion rot, float health, bool alive)
+    {
+        gameObject.SetActive(true);
+        enabled = true;
+
+        transform.SetPositionAndRotation(pos, rot);
+
+        currentHealth = health;
+        isDead = !alive;
+
+        if (agent != null)
+        {
+            agent.enabled = true;
+            agent.isStopped = false;
+            agent.ResetPath();
+        }
+
+        if (CurrentCover != null)
+        {
+            CurrentCover.isOccupied = false;
+            CurrentCover = null;
+        }
+
+        if (target == null && gameManager.instance != null && gameManager.instance.player != null)
+        {
+            target = gameManager.instance.player.transform;
+        }
+
+        if (alive)
+        {
+            stateMachine.Initialize(IdleState);
+        }
+        else
+        {
+            enabled = false;
+
+            if (agent != null)
+            {
+                agent.isStopped = true;
+                agent.enabled = false;
+            }
+
+            gameObject.SetActive(false);
+        }
     }
     public void SetHomePoint(Transform newHomePoint)
     {
