f9b3cddb31d04bf5ce9f4255c6ab3f2362252b3c
Joseph Downs <capt1anp3r53u5@gmail.com>
2026-03-16 19:19:58 -0500

    started note popup and menu navigation

---
+++ Echoes of Belleau/Assets/Scripts/note.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class note : MonoBehaviour
+{
+    [SerializeField] GameObject model;
+    [SerializeField] GameObject button;
+
+    bool playerInTrigger;
+    bool used;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Interact") && playerInTrigger)
+        {
+            model.SetActive(true);
+        }
+        if (Input.GetButtonDown("Cancel"))
+        {
+            model.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInTrigger = true;
+            button.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInTrigger = false;
+            button.SetActive(false);
+        }
+    }
+}
