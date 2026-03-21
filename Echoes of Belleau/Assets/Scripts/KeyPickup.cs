38c0da06f34fde3dab21e3e20add82abca1f71c0
Nicolas-Lowery <lowerynicolas@gmail.com>
2026-03-21 18:51:17 -0400

    Added Search Objective and tank sound

--- Echoes of Belleau/Assets/Scripts/KeyPickup.cs
+++ Echoes of Belleau/Assets/Scripts/KeyPickup.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 
 public class KeyPickup : MonoBehaviour
 {
+    [SerializeField] SearchObjective linkedSearchObjective;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             Door.Instance.isLocked = false;
+
+            if (linkedSearchObjective != null)
+                linkedSearchObjective.CompleteObjective();
         }
     }
 }
